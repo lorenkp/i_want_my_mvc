0ff3ff6e900856fd65b1b36029842c3f5bde2d3a
Loren Peterson <lorenkp@gmail.com>
2015-10-27 15:23:27 -0700

    Added render_template, render, and invoke_action methods to ControllerBase

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -1,33 +1,41 @@
+require 'active_support/inflector'
+require 'erb'
+
 class ControllerBase
   attr_reader :req, :res
-  attr_accessor :already_built_response
+  attr_accessor :already_built_response, :session
 
   def initialize(req, res, route_params = {})
     @req = req
     @res = res
     @params = Params.new(req, route_params)
+    @session = Session.new(res)
   end
 
   def redirect_to(url)
-    raise 'already rendered' if already_built_response?
+    raise 'already rendered' if already_built_response
     res.status = 302
     res['Location'] = url
-    @already_built_response = true
+    already_built_response = true
+    session.store_session(res)
+  end
+
+  def render_template(content, content_type)
+    raise 'already rendered' if already_built_response
+    res.content_type = content_type
+    res.body = content
+    already_built_response = true
+    session.store_session(res)
   end
 
   def render
-
-  end
-
-  def render_template
-
-  end
-
-  def session
-
+    template_contents =
+      File.read("app/views/#{self.class.name.underscore}/#{template_name}.html.erb")
+    render_template(ERB.new(template_contents).result(binding), 'text/html')
   end
 
   def invoke_action
-
+    send(name)
+    render(name) unless already_built_response
   end
 end
