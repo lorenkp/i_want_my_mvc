38ae08411dfda5a1ca794742ee3bc7cd4ca504bb
Loren Peterson <lorenkp@gmail.com>
2015-11-03 11:19:05 -0800

    ModelBase#insert #update ::where

--- lib/model_base.rb
+++ lib/model_base.rb
@@ -1,7 +1,7 @@
 require 'active_support/inflector'
 require 'pg'
 
-database = PG.new
+Database = PG.new
 
 class ModelBase
   def initialize(params)
@@ -24,7 +24,7 @@ class ModelBase
 
   def self.columns
     return @columns if @columns
-    cols = database.exec(<<-SQL)
+    cols = Database.exec(<<-SQL)
     SELECT
     column_name
     FROM
@@ -48,7 +48,7 @@ class ModelBase
   end
 
   def self.all
-    query_hash = database.exec(<<-SQL)
+    query_hash = Database.exec(<<-SQL)
     SELECT
     #{table_name}.*
     FROM
@@ -65,15 +65,66 @@ class ModelBase
     @attributes ||= {}
   end
 
+  def attribute_values
+    attributes.values
+  end
+
   def self.find(id)
-    result = database.exec(<<-SQL, [id])
+    result = Database.exec(<<-SQL, [id])
       SELECT
-        *
+       *
       FROM
         #{table_name}
       WHERE
-        id = $1
+       id = $1
     SQL
     result.empty? ? nil : new(result[0])
+  end
+
+  def insert
+    col_names = self.class.columns.drop(1).map(&:to_s).join(', ')
+    vals = (1..self.class.columns.length - 1)
+           .to_a.map { |el| '$' + el.to_s }.join(', ')
+    insertion = Database.each_params(<<-SQL, attribute_values.drop(1))
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{vals})
+      RETURNING
+        id
+    SQL
+    self.id = insertion[0]
+  end
+
+  def update
+    set_line = self.class.columns
+               .map { |col| col.to_s + '= ?' }.join(', ')
+    Database.exec_params(<<-SQL, attribute_values)
+    UPDATE
+      #{self.class.table_name}
+    SET
+      #{set_line}
+    WHERE
+      id = #{self.id}
+    SQL
+  end
+
+  def save
+    id.nil? ? insert : update
+  end
+
+  def self.where(params)
+    where_line = params.keys.map{ |key| key.to_s + ' = ?' }.join(' AND ')
+
+    results = DB.exec(<<-SQL, params.values)
+      SELECT
+       *
+      FROM
+       #{table_name}
+      WHERE
+       #{where_line}
+    SQL
+
+    parse_all(results)
   end
 end
